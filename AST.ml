79108cb22a1449a7cad3a2cee040961c027b425f
ZinebBouk <Zineb.boukhima07@gmail.com>
2022-05-09 01:08:37 +0200

    calcule

--- AST.ml
+++ AST.ml
@@ -1,3 +1,4 @@
+
 type expression_a =
     | Plus  of expression_a * expression_a
     | Moins of expression_a * expression_a
@@ -22,3 +23,39 @@ and print_AST form = let open Format in function
     | Neg    e    -> fprintf form "@[<2>%s@ %a@]" "Neg" print_AST e 
     | Num    n    -> fprintf form "@[<2>%s@ %F@]" "Num" n
 ;; 
+
+(* Fonction pour l'assembleur*)
+let rec assembleur_AST form = match form with
+    | Plus  (g,d) -> (assembleur_AST g)^"\n"^(assembleur_AST d)^"\n"^"AddiNb\n"
+    | Num    n    -> "CstNb "^string_of_float n
+    | Moins (g,d) -> (assembleur_AST g)^"\n"^(assembleur_AST d)^"\n"^"SubiNb\n"
+    | Mult  (g,d) -> (assembleur_AST g)^"\n"^(assembleur_AST d)^"\n"^"MultNb\n"
+    | Div  (g,d) -> (assembleur_AST g)^"\n"^(assembleur_AST d)^"\n"^"DiviNb\n"
+    | Mod  (g,d) -> (assembleur_AST g)^"\n"^(assembleur_AST d)^"\n"^"ModNb\n"
+    | Neg    e    -> (assembleur_AST e)^"\n"^"NegaNb\n"
+;; 
+
+(* Fonctions pour les calculs*)
+
+let print_result g d o = match o with
+    | "+." -> (g +. d)
+    | "-." -> (g -. d)
+    | "*." -> (g *. d)
+    | "/." -> (g /. d)
+    | "%." -> mod_float g d
+    | "-"  -> -. g
+    | _ -> 0.
+;;
+
+let rec resultat_AST form = match form with
+    | Num    n    -> n
+    | Plus  (g,d) -> print_result (resultat_AST g) (resultat_AST d) "+."
+    | Moins (g,d) -> print_result (resultat_AST g) (resultat_AST d) "-."
+    | Mult  (g,d) -> print_result (resultat_AST g) (resultat_AST d) "*."
+    | Div   (g,d) -> print_result (resultat_AST g) (resultat_AST d) "/."
+    | Mod   (g,d) -> print_result (resultat_AST g) (resultat_AST d) "%."
+    | Neg    e    -> print_result (resultat_AST e) (resultat_AST e) "-"
+;; 
+
+
+
--- main.ml
+++ main.ml
@@ -2,15 +2,13 @@
 
 let _ = (*main en OCaml*)
 try
-let argc = Array.length Sys.argv in
-let lexbuf = if (argc == 2) then let file = open_in Sys.argv.(1) in Lexing.from_channel file else
-Lexing.from_channel stdin in (*lexeur lancé sur stdin*)
+let lexbuf =  Lexing.from_channel stdin in (*lexeur lancé sur stdin*)
 while true do (*on ne s'arrête pas*)
-Parseur.main Lexeur.token lexbuf (*parseur une ligne*)
-|> Format.printf "%a\n%!" AST.print_AST ;
+(Parseur.main Lexeur.token lexbuf (*parseur une ligne*)
+|> AST.resultat_AST) |> Printf.printf "%F\n%!" ;
 done
 with
-| End_of_file -> close_in (open_in Sys.argv.(1))
+(*| End_of_file -> close_in (open_in Sys.argv.(1)); close_out destination*)
 | Lexeur.Eof -> exit 0 (*impossible*)
 | Lexeur.TokenInconu (*erreur de lexing*)
 | Parsing.Parse_error -> (*erreur de parsing*)
